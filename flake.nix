f9bd565d3a53b86a0026688291236eaba897a559
Josef <me@jhofer.de>
2024-11-03 01:49:43 +0100

    chore: remove inputs imported by base

--- flake.nix
+++ flake.nix
@@ -2,9 +2,6 @@
   description = "personal nix configuration";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    darwin.url = "github:lnl7/nix-darwin/master";
-    home-manager.url = "github:nix-community/home-manager";
     base.url = "github:ojsef39/nix-base/dev";
   };
 
