430b49af5328170fd9a6089bf20b865cce63f661
Josef <me@jhofer.de>
2024-11-03 16:46:02 +0100

    fix flake

--- flake.nix
+++ flake.nix
@@ -5,50 +5,41 @@
     base.url = "github:ojsef39/nix-base/dev";
   };
 
-  outputs = { self, base }: {
-    let
-      vars = {
-        user = "josefhofer";
-        email = "me@jhofer.de"
+  outputs = { self, base }: let
+    vars = {
+      user = "josefhofer";
+      email = "me@jhofer.de";
+    };
+  in {
+    darwinConfigurations = {
+      "mac" = base.inputs.darwin.lib.darwinSystem {
+        system = "aarch64-darwin";  # or x86_64-darwin
+        modules = base.outputs.sharedModules ++ base.outputs.macModules ++ [
+          {
+            programs.zsh = {
+              enable = true;
+            };
+          }
+        ];
       };
+    };
 
-    in { 
-      darwinConfigurations = {
-        # Simplified to just "mac"
-        "mac" = base.inputs.darwin.lib.darwinSystem {
-          system = "aarch64-darwin";  # or x86_64-darwin
-          modules = base.outputs.sharedModules ++ base.outputs.macModules ++ [
-            # Mac-specific shell customizations
-            {
-              programs.zsh = {
-                enable = true;
-              };
-            }
-          ];
-        };
-
-      };
-
-      # nixosConfigurations = {
-      #   "linux" = nixpkgs.lib.nixosSystem {
-      #     system = "x86_64-linux";
-      #     modules = [
-      #       base.baseModules
-      #       # Linux-specific shell customizations
-      #       {
-      #         programs.bash = {
-      #           enable = true;
-      #           # Add Linux-specific aliases or settings
-      #           shellAliases = {
-      #             # Linux-specific aliases
-      #             open = "xdg-open";
-      #             # ... more Linux-specific aliases
-      #           };
-      #         };
-      #       }
-      #     ];
-      #   };
-      # };
-    }
+    # Uncomment and adjust the following section if needed
+    # nixosConfigurations = {
+    #   "linux" = nixpkgs.lib.nixosSystem {
+    #     system = "x86_64-linux";
+    #     modules = [
+    #       base.baseModules
+    #       {
+    #         programs.bash = {
+    #           enable = true;
+    #           shellAliases = {
+    #             open = "xdg-open";
+    #           };
+    #         };
+    #       }
+    #     ];
+    #   };
+    # };
   };
 }
