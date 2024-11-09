94880c20a81f41ae9a7547605c78db714f7f495e
Josef <me@jhofer.de>
2024-11-09 21:04:06 +0100

    feat: enable mas apps

--- hosts/darwin/homebrew.nix
+++ hosts/darwin/homebrew.nix
@@ -2,33 +2,32 @@
 {
   # Homebrew for macOS-specific and unavailable packages
   homebrew = {
-   taps = [];
+    taps = [];
 
     # Mac App Store apps
     masApps = {
-        # "CrystalFetch" = 6454431289;
-        # "Parcel" = 639968404;
-        # "Keynote" = 409183694;
-        # "Final Cut Pro" = 424389933;
-        # "Usage" = 1561788435;
-        # "Motion" = 434290957;
-        # "The Unarchiver" = 425424353;
-        # "TestFlight" = 899247664;
-        # "Ground News" = 1324203419;
-        # "Book Tracker" = 1496543317;
-        # "Pages" = 409201541;
-        # "Reeder" = 1529448980;
-        # "Goodnotes" = 1444383602;
-        # "Numbers" = 409203825;
-        # "Pixelmator Pro" = 1289583905;
-        # "Tailscale" = 1475387142;
-      };
+      "CrystalFetch" = 6454431289;
+      "Parcel" = 639968404;
+      "Keynote" = 409183694;
+      "Final Cut Pro" = 424389933;
+      "Usage" = 1561788435;
+      "Motion" = 434290957;
+      "The Unarchiver" = 425424353;
+      "TestFlight" = 899247664;
+      "Ground News" = 1324203419;
+      "Book Tracker" = 1496543317;
+      "Pages" = 409201541;
+      "Reeder" = 1529448980;
+      "Goodnotes" = 1444383602;
+      "Numbers" = 409203825;
+      "Pixelmator Pro" = 1289583905;
+      "Tailscale" = 1475387142;
+    };
 
     # Homebrew formulae (CLI tools)
     brews = [
       # "docx2pdf" ##TODO: Needs tap
       "iperf3"
-
     ];
 
     # macOS-specific apps and those not available/stable in nixpkgs
