f81d187beaa9bcdff9c50a6bacd17b0673df9b97
Josef Hofer <me@jhofer.de>
2024-11-10 16:32:27 +0100

    refactor: remove unnecessary session variables from zsh config

--- hosts/shared/programs/shell/default.nix
+++ hosts/shared/programs/shell/default.nix
@@ -1,13 +1,6 @@
 { pkgs, lib, vars, ... }: {
 
   programs.zsh = {
-    # Environment variables
-    sessionVariables = {
-      PATH = lib.concatStringsSep ":" [
-        "$PATH"
-      ];
-    };
-
     initExtra = ''
       # Source MEGA completion
       # source /Applications/MEGAcmd.app/Contents/MacOS/megacmd_completion.sh
