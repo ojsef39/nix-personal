031ea164992d9bbc70575f7ee6abfc7e4c60ce87
Josef Hofer <me@jhofer.de>
2024-11-23 14:34:23 +0100

    fix: temporary nixcord fix

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "personal nix configuration";
 
   inputs = {
-    base.url = "github:ojsef39/nix-base";
+    base.url = "github:ojsef39/nix-base/dev";
     # base.url = "/Users/josefhofer/CodeProjects/github.com/ojsef39/nix-base/";
   };
 
