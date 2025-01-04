5d88ba9d6bf6e2a56ddf42faa0cddbd06e29b38b
Josef Hofer <me@jhofer.de>
2025-01-04 14:30:56 +0100

    feat(shell): add talosctl.zsh script for credential management
    Implemented a new utility script to handle Talos context setup, including fetching credentials using 1Password and configuring Talosctl.

--- hosts/shared/programs/shell/default.nix
+++ hosts/shared/programs/shell/default.nix
@@ -4,9 +4,29 @@
     initExtra = ''
       # Source MEGA completion
       # source /Applications/MEGAcmd.app/Contents/MacOS/megacmd_completion.sh
+
+      # Source additional scripts
+      if [ -d $HOME/.zsh_scripts ]; then
+        for file in $HOME/.zsh_scripts_local/*.zsh; do
+          source $file
+        done
+      fi
+
     '';
 
     # Aliases
-    shellAliases = {};
+    shellAliases = {
+      talos = "talosctl";
+    };
   };
+
+  home = {
+    file = {
+      ".zsh_scripts_local/" = {
+        recursive = true;
+        source = ./zsh_scripts;
+      };
+    };
+  };
+
 }
