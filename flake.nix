af9c8a675e8f5b4c3aa3f531b848603ee62fb952
Josef Hofer <me@jhofer.de>
2025-04-26 22:35:19 +0200

    chore: chores (#477)
    * fix: deprecations (#473)
    
    ref https://github.com/ojsef39/nix-base/issues/91
    
    * chore(flake): add example lazygit variables

--- flake.nix
+++ flake.nix
@@ -10,7 +10,7 @@
     nixcord.follows = "base/nixcord";
   };
 
-  outputs = { self, base, ... }: let
+  outputs = { base, ... }: let
     vars = {
       user = "josefhofer";
       full_name = "Josef Hofer";
@@ -18,6 +18,12 @@
       git = {
         ghq = "CodeProjects";
         url = "";
+        lazy = {
+          # authorColors = ''
+          #   "renovate[bot]": "#f4dbd6" # Rosewater
+          #   "dependabot[bot]": "#f4dbd6" # Rosewater
+          # '';
+          };
         nix = "$HOME/${vars.git.ghq}/github.com/ojsef39/nix-personal";
       };
       kitty.project_selector = "~/.config";
